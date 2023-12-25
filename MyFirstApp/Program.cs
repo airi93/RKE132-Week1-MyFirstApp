2925f31e26328ccee4fc7e8ebbc67cfe735327a0
Irina Belik. <ibelik@tktk.ee>
2023-12-25 12:59:29 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//rakendus palub kasutajal sisestada tema nime
+//rakendus tervutab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");
+//string - sõne
+string userName;
+
+userName = Console.ReadLine(); /*input*/
+
+Console.WriteLine("Hello" + "," + userName); /*output*/
+
+//string - interpolation
+
+Console.WriteLine($"Hello, {userName} !");
+
+
+
